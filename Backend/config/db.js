a32d32d4a41ab225aac4faa78f953e927aa9b5de
divyanshu <divyanshu65493@gmail.com>
2025-05-26 12:36:26 +0530

    register done

---
+++ Backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+
+  const connectDb = async()=>{
+    mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected successfully');
+}   ).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+  }
+  module.exports = connectDb;
